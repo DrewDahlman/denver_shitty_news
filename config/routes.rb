eb591e402dfb8463c6579ff30bfda485893312fd
DrewDahlman <info@drewdahlman.com>
2015-07-21 21:14:25 -0600

    burn it all

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-
-  ## Everything else
-  root "site#index"
-  get "about" => "site#about"
-  get "update" => "site#update" ## Just clears the cache real quick
-  get "/featured/:hash_url" => "article#featured"
-  get "/:category/:hash_url" => "article#show"
-end
--- db/migrate/20150722022612_add_category_to_top_headline.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoryToTopHeadline < ActiveRecord::Migration
-  def change
-    add_column :top_headlines, :category_id, :integer
-  end
-end
